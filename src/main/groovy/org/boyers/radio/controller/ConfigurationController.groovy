88dfae3adfb805a1e2aeb51450e8081ac9e21587
nobody <6844222+mboyers@users.noreply.github.com>
2017-09-04 18:01:19 +0000

    removed temporary methods

--- src/main/groovy/org/boyers/radio/controller/ConfigurationController.groovy
+++ src/main/groovy/org/boyers/radio/controller/ConfigurationController.groovy
@@ -1,9 +1,6 @@
 package org.boyers.radio.controller
 
-import groovy.json.JsonBuilder
-import groovy.json.JsonSlurper
 import groovy.util.logging.Slf4j
-import org.boyers.radio.config.TunePoint
 import org.boyers.radio.player.Player
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
@@ -20,21 +17,6 @@ class ConfigurationController {
     @RequestMapping(value = '/')
     @ResponseBody
     String index() {
-        writeJson()
-        readJson()
         player.nowPlaying
-    }
-
-    void writeJson() {
-        TunePoint tp = new TunePoint(displayValue: 10, potentiometerValue: 23)
-        Map<Integer, TunePoint> tpm = [:]
-        tpm.put(10, tp)
-        new File("tuner").write(new JsonBuilder(tpm).toPrettyString())
-    }
-
-    void readJson() {
-        def inputFile = new File("tuner")
-        def InputJSON = new JsonSlurper().parseText(inputFile.text)
-        InputJSON.each{ println it }
     }
 }
