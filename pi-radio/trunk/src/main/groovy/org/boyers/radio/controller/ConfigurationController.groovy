b8147ed7e4ce047993c27f5c2f28e0f872c93ce1
nobody <nobody@a34b6255-1323-2149-9553-26c9e14722b4>
2017-09-10 14:01:27 +0000

    tried to set up strategy for making a main page made up of ajax fragments

--- pi-radio/trunk/src/main/groovy/org/boyers/radio/controller/ConfigurationController.groovy
+++ pi-radio/trunk/src/main/groovy/org/boyers/radio/controller/ConfigurationController.groovy
@@ -5,7 +5,6 @@ import org.boyers.radio.player.Player
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
 
 @Slf4j
 @Controller
@@ -15,8 +14,7 @@ class ConfigurationController {
     Player player
 
     @RequestMapping(value = '/')
-    @ResponseBody
     String index() {
-        player.nowPlaying
+        'index'
     }
 }
