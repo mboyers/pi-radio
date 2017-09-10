7163e2764d0fbf2107c72f718a67288c0bbb524a
nobody <nobody@a34b6255-1323-2149-9553-26c9e14722b4>
2017-09-10 20:26:50 +0000

    formatted now playing section

--- pi-radio/trunk/src/main/groovy/org/boyers/radio/controller/IndexController.groovy
+++ pi-radio/trunk/src/main/groovy/org/boyers/radio/controller/IndexController.groovy
@@ -4,17 +4,19 @@ import groovy.util.logging.Slf4j
 import org.boyers.radio.player.Player
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Slf4j
 @Controller
-class ConfigurationController {
+class IndexController {
 
     @Autowired
     Player player
 
     @RequestMapping(value = '/')
-    String index() {
+    String index(Model model) {
+        model.addAttribute('nowPlayingSong', player.nowPlaying)
         'index'
     }
 }
