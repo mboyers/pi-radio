92f0b8f98f11088caf5bde1c997064632ce9d037
nobody <nobody@a34b6255-1323-2149-9553-26c9e14722b4>
2017-09-12 03:01:40 +0000

    added ability to test stations

---
+++ pi-radio/trunk/src/main/groovy/org/boyers/radio/controller/StationTesterController.groovy
@@ -0,0 +1,33 @@
+package org.boyers.radio.controller
+
+import groovy.util.logging.Slf4j
+import org.boyers.radio.actor.Station
+import org.boyers.radio.player.Player
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Slf4j
+@Controller
+@RequestMapping('/testStation')
+class StationTesterController {
+
+    @Autowired
+    Player player
+
+    @RequestMapping('')
+    String index() {
+        'stationTester'
+    }
+
+    @RequestMapping(value = '/play', method = RequestMethod.POST)
+    @ResponseBody
+    void playUserSuppliedStation(@RequestBody  String stationUri) {
+        Station station = new Station(uri: stationUri, name: 'User Supplied Station for Testing')
+        player.playStation(station)
+        log.info('Playing user supplied station {}', stationUri)
+    }
+}
