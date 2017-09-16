23dc70a8a3a1163cff3949eb10d8f29902f7de07
nobody <6844222+mboyers@users.noreply.github.com>
2017-09-16 15:13:56 +0000

    configurable stations is now functional, needs a little cleanup still

--- src/main/groovy/org/boyers/radio/controller/StationConfigurationController.groovy
+++ src/main/groovy/org/boyers/radio/controller/StationConfigurationController.groovy
@@ -2,12 +2,14 @@ package org.boyers.radio.controller
 
 import groovy.util.logging.Slf4j
 import org.boyers.radio.actor.Station
+import org.boyers.radio.config.StationConfigurationPersister
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Slf4j
 @Controller
@@ -21,6 +23,9 @@ class StationConfigurationController {
     @Autowired
     List<Station> stations
 
+    @Autowired
+    StationConfigurationPersister stationConfigurationPersister
+
     @RequestMapping(value = '', method = RequestMethod.GET)
     String presentSectionConfigurationSection(Model model) {
         model.addAttribute('stations', buildStationList())
@@ -28,8 +33,12 @@ class StationConfigurationController {
     }
 
     @RequestMapping(value = '', method = RequestMethod.POST)
+    @ResponseBody
     String saveStation(Station station) {
-        'station-configuration'
+        replaceStationInList(station)
+        stationConfigurationPersister.saveStations(stations)
+        log.info('Saved station: {}', station)
+        'saved station'
     }
 
     /**
@@ -43,7 +52,7 @@ class StationConfigurationController {
             if (station) {
                 stationList.add(station)
             } else {
-                stationList.add(new Station())
+                stationList.add(new Station(dialPosition: it))
             }
         }
         stationList
@@ -52,6 +61,18 @@ class StationConfigurationController {
     private Station findStationAtTunePoint(Integer tunePointValue) {
         stations.find {
             it.dialPosition == tunePointValue
+        }
+    }
+
+    private void replaceStationInList(Station station) {
+        Integer index = stations.findIndexOf {
+            it.dialPosition == station.dialPosition
+        }
+        
+        if (index != -1) {
+            stations.putAt(index, station)
+        } else {
+            stations.add(station)
         }
     }
 }
