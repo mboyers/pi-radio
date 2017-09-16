30fddff3d4f405c14e130d2a24431e5c97d813d9
nobody <nobody@a34b6255-1323-2149-9553-26c9e14722b4>
2017-09-16 18:19:34 +0000

    moved now playing stuff to its own controller and view

--- pi-radio/trunk/src/main/groovy/org/boyers/radio/controller/IndexController.groovy
+++ pi-radio/trunk/src/main/groovy/org/boyers/radio/controller/IndexController.groovy
@@ -1,23 +1,15 @@
 package org.boyers.radio.controller
 
 import groovy.util.logging.Slf4j
-import org.boyers.radio.player.Player
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Slf4j
 @Controller
 class IndexController {
 
-    @Autowired
-    Player player
-
     @RequestMapping(value = '/')
-    String index(Model model) {
-        model.addAttribute('nowPlayingStation', player.nowPlayingStation)
-        model.addAttribute('nowPlayingSong', player.nowPlayingSong)
+    String index() {
         'index'
     }
 }
