baf434f7a5e8e6d2b93aee0f628b3e3393dde4b7
nobody <6844222+mboyers@users.noreply.github.com>
2017-09-17 21:16:20 +0000

    added tune point 49

--- src/main/groovy/org/boyers/radio/config/Config.groovy
+++ src/main/groovy/org/boyers/radio/config/Config.groovy
@@ -54,7 +54,7 @@ class Config {
     @Bean(name = 'availableTunePoints')
     List<Integer> getAvailableTunePoints() {
         // Though this is currently just a series of even numbers, in the future it might be less predictable (might want to include 47 for example)
-        [30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50]
+        [30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 49, 50]
     }
 
     @Bean
