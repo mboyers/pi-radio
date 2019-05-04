318891ab98866d34da2ca313c34cf1792d707f30
mboyers <mikeboyers@gmail.com>
2019-05-04 16:18:01 -0400

    removed un-needed component scan annotation

--- src/main/groovy/org/boyers/radio/Application.groovy
+++ src/main/groovy/org/boyers/radio/Application.groovy
@@ -2,12 +2,10 @@ package org.boyers.radio
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
-@ComponentScan('org.boyers.radio')
 class Application {
 
     static void main(String[] args) {
