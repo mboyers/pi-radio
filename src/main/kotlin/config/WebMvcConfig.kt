9f0a655b8663242d003277b7162090568638b200
mboyers <mikeboyers@gmail.com>
2024-08-11 09:00:58 -0400

    downgraded spring boot for now to match pi radio's jvm, reworked api for new front end

--- src/main/kotlin/config/WebMvcConfig.kt
+++ src/main/kotlin/config/WebMvcConfig.kt
@@ -9,18 +9,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebMvcConfig: WebMvcConfigurer {
     
-    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        registry.addResourceHandler("/html/**").addResourceLocations("classpath:/html/")
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/")
-        registry.addResourceHandler("/favicon.png").addResourceLocations("classpath:/images/favicon.png")
-    }
-
-    override fun addViewControllers(registry: ViewControllerRegistry) {
-        registry.addViewController("/").setViewName("forward:/html/index.html")
-    }
-
-    override fun configureDefaultServletHandling(configurer: DefaultServletHandlerConfigurer) {
-        configurer.enable()
-    }
+//    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+//        registry.addResourceHandler("/html/**").addResourceLocations("classpath:/html/")
+//        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/")
+//        registry.addResourceHandler("/favicon.png").addResourceLocations("classpath:/images/favicon.png")
+//    }
+//
+//    override fun addViewControllers(registry: ViewControllerRegistry) {
+//        registry.addViewController("/").setViewName("forward:/html/index.html")
+//    }
+//
+//    override fun configureDefaultServletHandling(configurer: DefaultServletHandlerConfigurer) {
+//        configurer.enable()
+//    }
 
 }
--- src/main/kotlin/controller/CalibrationController.kt
+++ src/main/kotlin/controller/CalibrationController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/calibrate")
+@RequestMapping("/api/calibrate")
 class CalibrationController @Autowired constructor(
         val availableTunePoints: List<Int>,
         val tunePoints: MutableList<TunePoint>,
